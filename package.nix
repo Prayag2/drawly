c7cc6321e9236762a98c9aff86ba8a8d14a54c59
quarterstar <quarterstar@proton.me>
2025-11-15 20:05:01 +0100

    feat: install desktop entry for nix package

--- package.nix
+++ package.nix
@@ -9,17 +9,6 @@
   qt6,
 }:
 
-let
-  icon = makeDesktopItem {
-    name = "Drawy";
-    exec = "drawy";
-    icon = "drawy";
-    comment = "Your handy, infinite, brainstorming tool.";
-    desktopName = "Drawy";
-    genericName = "Drawy";
-    categories = [ "Graphics" ];
-  };
-in
 stdenv.mkDerivation rec {
   pname = "drawy";
   version = "1.0.0";
@@ -36,10 +25,38 @@ stdenv.mkDerivation rec {
     wrapQtAppsHook
   ];
 
+  desktopItem = makeDesktopItem {
+    name = "Drawy";
+    exec = "drawy";
+    icon = "drawy";
+    comment = meta.description;
+    desktopName = "Drawy";
+    genericName = "Drawy";
+    categories = [ "Graphics" ];
+  };
+
   buildInputs = [
     qtEnv
     qt6.qtbase
   ];
+
+  installPhase = ''
+    runHook preInstall
+
+    install -Dm755 drawy $out/bin/drawy
+
+    for desktop_file in "$desktopItem/share/applications/*"; do
+      install -Dm644 $desktop_file $out/share/applications/$(basename $desktop_file)
+    done
+
+    install -Dm644 ${./assets/logo-16.png} $out/share/icons/hicolor/16x16/apps/drawy.png
+    install -Dm644 ${./assets/logo-32.png} $out/share/icons/hicolor/32x32/apps/drawy.png
+    install -Dm644 ${./assets/logo-256.png} $out/share/icons/hicolor/256x256/apps/drawy.png
+    install -Dm644 ${./assets/logo-512.png} $out/share/icons/hicolor/512x512/apps/drawy.png
+    install -Dm644 ${./assets/logo.svg} $out/share/icons/hicolor/scalable/apps/drawy.svg
+
+    runHook postInstall
+  '';
 
   meta = with lib; {
     description = "Your handy, infinite, brainstorming tool";
